f9d7f7a419b9196fd1c6f30131fba07808d83972
Ippei Shimizu <ippei.shimizu.32@gmail.com>
2025-03-02 13:07:59 +0900

    add: ルーティング

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,19 @@ app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Start Server" });
 });
 
+app.get("/hello", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.get("/users/:id", (req: Request, res: Response) => {
+  res.send({ id: req.params.id });
+});
+
+app.get("/search", (req: Request, res: Response) => {
+  const query = req.query.q;
+  res.send(`search: ${query}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
